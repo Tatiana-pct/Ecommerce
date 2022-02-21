f17a4820895c1259ecbd793e50394d5909ef96ac
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2022-02-21 14:54:23 +0100

    importation des images dans voyager

--- database/migrations/2022_02_18_144750_create_products_table.php
+++ database/migrations/2022_02_18_144750_create_products_table.php
@@ -21,6 +21,8 @@ class CreateProductsTable extends Migration
             $table->integer('price');
             $table->text('description');
             $table->unsignedBigInteger('category_id')->index();
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
             $table->timestamps();
         });
     }
